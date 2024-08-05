d09db5f63b59fba2a09da6fe3551e380a943b047
heloisa-lopes <heloisalopesalbano@gmail.com>
2024-08-05 11:15:14 -0300

    corrigindo erro da função retorno ao nome

--- core/models/marca.py
+++ core/models/marca.py
@@ -5,4 +5,4 @@ class Marca(models.Model):
     nasciolalidade = models.CharField(max_length=50)
 
     def __str__(self):
-        return f"{self.descricao} - {self.id}"
+        return f"{self.nome.upper()} - {self.id}"
